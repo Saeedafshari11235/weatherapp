b13c376c3aee94d5864654337fe7f3093d33633a
Saeedafshari11235 <msafshari11235@gmail.com>
2022-08-20 18:44:13 +0430

    adding cities

---
+++ script/script.js
@@ -0,0 +1,7 @@
+let cities={
+    tehran:{city:"Tehran",temp:"25°C",humidity:"6%",windspeed:"10km",weather:"Cloudy"},
+    london:{city:"London",temp:"18°C",humidity:"55%",windspeed:"20km",weather:"Rainy"},
+    tokyo:{city:"Tokyo",temp:"20°C",humidity:"3%",windspeed:"2km",weather:"Sunny"},
+    newyork:{city:"NewYork",temp:"25°C",humidity:"80%",windspeed:"30km",weather:"Thunder"},
+    beijing:{city:"Beijing",temp:"9°C",humidity:"30%",windspeed:"0km",weather:"Foggy"}
+}
